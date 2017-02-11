7df9932286b4cb054be833f79c78f1af12bc38b1
Pavan <pavanvenkatesh94@gmail.com>
2017-02-11 23:49:13 +0530

    basic framework and working implemented

--- horizontalpicker/src/main/java/com/goodiebag/horizontalpicker/HorizontalPicker.java
+++ horizontalpicker/src/main/java/com/goodiebag/horizontalpicker/HorizontalPicker.java
@@ -3,7 +3,14 @@ package com.goodiebag.horizontalpicker;
 import android.content.Context;
 import android.content.res.ColorStateList;
 import android.content.res.TypedArray;
+import android.graphics.Rect;
+import android.support.annotation.ColorInt;
+import android.support.annotation.DrawableRes;
 import android.util.AttributeSet;
+import android.util.Log;
+import android.view.Gravity;
+import android.view.MotionEvent;
+import android.view.View;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.TextView;
@@ -15,12 +22,14 @@ import java.util.List;
  * Created by Kai on 11/02/17.
  */
 
-public class HorizontalPicker extends LinearLayout{
+public class HorizontalPicker extends LinearLayout implements View.OnTouchListener {
     private final float DENSITY = getContext().getResources().getDisplayMetrics().density;
     //attrs
     private CharSequence values[];
-    private ColorStateList textColor;
-    private float textSize;
+    @DrawableRes
+    private int backgroundSelector = R.drawable.selector_background;
+    private int colorSelector = R.drawable.selector_tv;
+    private int textSize = 12;
 
     private List<TextView> tvList =  new ArrayList<>();
 
@@ -40,13 +49,12 @@ public class HorizontalPicker extends LinearLayout{
 
     private void initAttributes(Context context, AttributeSet attrs, int defStyleAttr) {
         if(attrs != null){
+            textSize *= DENSITY;
             final TypedArray array = context.obtainStyledAttributes(attrs, R.styleable.HorizontalPicker, defStyleAttr, 0);
             values = array.getTextArray(R.styleable.HorizontalPicker_values);
-            textColor = array.getColorStateList(R.styleable.HorizontalPicker_android_textColor);
-            if (textColor == null) {
-                textColor = ColorStateList.valueOf(0xFF000000);
-            }
-            float textSize = array.getDimension(R.styleable.HorizontalPicker_android_textSize, -1);
+            backgroundSelector = array.getResourceId(R.styleable.HorizontalPicker_backgroundSelector, backgroundSelector);
+            colorSelector = array.getResourceId(R.styleable.HorizontalPicker_textColorSelector, colorSelector);
+            textSize = (int) array.getDimension(R.styleable.HorizontalPicker_textSize, textSize);
 
             array.recycle();
         }
@@ -56,17 +64,54 @@ public class HorizontalPicker extends LinearLayout{
     private void initViews(Context context, AttributeSet attrs, int defStyleAttr) {
         //Linear Layout
         this.setOrientation(HORIZONTAL);
-        this.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        //this.setLayoutParams(new LayoutParams(ViewGroup.LayoutParams.MATCH_PARENT, ViewGroup.LayoutParams.WRAP_CONTENT));
+        this.setOnTouchListener(this);
         //TextViews
         for(int i = 0; i < values.length; i++){
             TextView tv = new TextView(context);
-            if(textSize != -1)
-                tv.setTextSize(textSize);
-            tv.setTextColor(textColor);
+            tv.setLayoutParams(new LinearLayout.LayoutParams(0, ViewGroup.LayoutParams.WRAP_CONTENT,1.0f));
+            tv.setTextSize(textSize);
+            tv.setTextColor(getResources().getColorStateList(colorSelector));
+            tv.setBackgroundResource(backgroundSelector);
+            tv.setGravity(Gravity.CENTER);
             tv.setText(values[i]);
-
+            this.addView(tv);
+            tvList.add(tv);
         }
+
     }
 
+    public void selectOneAndDeselectTheRest(View view){
+        for(int i = 0 ; i < values.length ; i++){
+            tvList.get(i).setSelected(view == (tvList.get(i)));
+        }
 
+    }
+
+    @Override
+    public boolean onTouch(View view, MotionEvent motionEvent) {
+        switch (motionEvent.getAction()) {
+            case MotionEvent.ACTION_DOWN:
+            case MotionEvent.ACTION_MOVE:
+                int x = (int) motionEvent.getX();
+                int y = (int) motionEvent.getY();
+                Rect hitRect = new Rect();
+                View v;
+                for (int i = 0; i < tvList.size(); i++) {
+                    v = this.getChildAt(i);
+                    v.getHitRect(hitRect);
+                    if (hitRect.contains(x, y)) {
+                        selectOneAndDeselectTheRest(v);
+                        Log.d("touched", "gotView");
+                        break;
+                    }
+                }
+                Log.d("touched", "here");
+                break;
+            default:
+                break;
+        }
+
+        return true;
+    }
 }
